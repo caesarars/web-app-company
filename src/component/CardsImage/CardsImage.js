e0d5933394f11de1bab12750fb27e62ff81acc80
Caesar Arssetya <38849354+caesarars@users.noreply.github.com>
2023-12-28 16:54:58 +0700

    refactor style cards image

--- src/component/CardsImage/CardsImage.js
+++ src/component/CardsImage/CardsImage.js
@@ -7,14 +7,16 @@ class CardsImage extends React.Component {
 
         const { imageUrl, title, project } = this.props;
 
+        const styleCards = {width: "18rem",  minWidth: "368px",minHeight: "640px", border:"none", marginTop:"32px", overflow:"hidden", borderRadius:"16px"}
+
         return (
             <div>
-                <div className="card" style={{width: "18rem",  minWidth: "368px",mineight: "591px", border:"none", marginTop:"32px", overflow:"hidden"}}>
+                <div className="card" style={styleCards}>
                     <img src={imageUrl} className="card-img-top" alt="..."/>
                     <div className="card-body" style={{backgroundColor:"#f2f2f2", textAlign:"left"}}>
-                        <p className="card-title">{title}</p>
+                        <p className="card-title" style={{fontSize:"16px"}}>{title}</p>
                         <h5 className="card-title" style={{fontSize:"24px" , fontWeight:"700", minWidth:"368px", textWrap:"nowrap",overflow:"hidden"}}>{project}</h5>
-                        <p className="card-text" style={{fontSize:"16px"}}>
+                        <p className="card-text" style={{fontSize:"16px", textAlign:"justify"}}>
                             Lorem ipsum dolor sit amet consectetur. Mattis sed orci turpis euismod id eu sem. Proin urna cursus dolor nunc id ac mal. Lorem ipsum dolor sit amet consectetur. Mattis sed orci turpis euismod id eu sem. Proin urna cursus dolor nunc id ac mal
                         </p>
                     </div>
